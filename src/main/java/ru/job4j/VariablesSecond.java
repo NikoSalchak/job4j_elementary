41d6b6e0430e376ce81759c43cb9d3810894ef62
NikoSalchak <nikosalchak@gmail.com>
2023-08-09 22:06:56 +0700

    1. Определите тип данных для переменных. [#172634]

---
+++ src/main/java/ru/job4j/VariablesSecond.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class VariablesSecond {
+    public static void main(String[] args) {
+        byte age = 27;
+        System.out.println("Возраст " + age + " лет");
+        char sex = 'М';
+        System.out.println("Пол " + sex);
+        float height = 1.68f;
+        System.out.println("Рост " + height + " метров");
+        short brickHouse = 28458;
+        System.out.println("Количество кирпичей для постройки дома 10*10 " + brickHouse + " кирпичей");
+        int bacteriaInYougurt = 107;
+        System.out.println("Количество лактобактерий в йогурте " + bacteriaInYougurt + " КОЕ/г");
+        long moonDistance = 384400;
+        System.out.println("Расстояние от земли до луны " + moonDistance + " км");
+        boolean workPermit = true;
+        System.out.println("имеется разрешение на работу " + workPermit);
+        long fileSize = 17486951;
+        System.out.println("размер файла А " + fileSize + " байт");
+    }
+}
