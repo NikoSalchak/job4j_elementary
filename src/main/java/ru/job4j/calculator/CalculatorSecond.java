57cbb687aaa1b2ba364c8056982ecd198a38d5d1
NikoSalchak <nikosalchak@gmail.com>
2023-08-11 23:11:57 +0700

    3.1. Переназначение переменной [#232354]

---
+++ src/main/java/ru/job4j/calculator/CalculatorSecond.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculator;
+
+public class CalculatorSecond {
+    public static void main(String[] args) {
+        int size = 10;
+        System.out.println("size = " + size);
+        size += 90;
+        System.out.println("size = " + size);
+        size -= 5;
+        System.out.println("size = " + size);
+    }
+}
