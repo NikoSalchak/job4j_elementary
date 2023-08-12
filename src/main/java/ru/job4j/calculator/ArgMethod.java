fdb8688af587f9f8ec410c3e55f1962e0675e36a
NikoSalchak <nikosalchak@gmail.com>
2023-08-12 11:55:03 +0700

    4.1. Аргументы [#295521]

---
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+
+    public static void main(String[] args) {
+        ArgMethod.hello("Job4j", 6);
+        ArgMethod.hello("Job4j", 6);
+        ArgMethod.hello("Job4j", 6);
+        ArgMethod.hello("Job4j", 6);
+    }
+}
