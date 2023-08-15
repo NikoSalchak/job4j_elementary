da66b902b974e3ac0b86470ee5fa54e35ef7c498
NikoSalchak <nikosalchak@gmail.com>
2023-08-15 11:53:12 +0700

    9.2. Преобразование примитивных типов [#383106]

--- src/main/java/ru/job4j/TypesConversationTwo.java
+++ src/main/java/ru/job4j/TypesConversationTwo.java
@@ -2,24 +2,19 @@ package ru.job4j;
 
 public class TypesConversationTwo {
     public static void main(String[] args) {
-        // 1 выражение
         long l = 129;
         byte b = (byte) l;
         System.out.print("Преобразования с потерей данных: 1 выражение " + b + "; ");
-        //2 выражение
         float f = 12358.7f;
         int i = 45981;
         short rsl = (short) (f + i);
         System.out.print(" 2 выражение " + rsl + "; ");
-        //4 выражение
         double d = 121.19;
         byte b2 = (byte) d;
         System.out.println(" 4 выражение " + b2 + "; ");
-        //3 Выражение
         char c = 45000;
         float i2 = c;
         System.out.print("Преобразования без потери данных: 3 выражение " + i2 + "; ");
-        // 5 выражение
         short s = 1500;
         char c2 = (char) s;
         double d2 = c2;
