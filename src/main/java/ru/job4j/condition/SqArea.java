856df33b6bb9bb50d6533e09ba8ab83ec0955f0e
NikoSalchak <nikosalchak@gmail.com>
2023-08-17 22:51:13 +0700

    4. Тесты для стороны прямоугольника. [#159460]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -12,7 +12,9 @@ public class SqArea {
     public static void main(String[] args) {
         double result1 = square(6, 2);
         double result2 = square(4, 1);
+        double result3 = square(10, 4);
         System.out.println("Если P = 6; k = 2; то S = " + result1);
         System.out.println("Если P = 4; k = 1; то S = " + result2);
+        System.out.println("Если P = 10; k = 4; то S = " + result3);
     }
 }
