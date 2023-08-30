8c00730cbf73dfcfe2334c4a32c58092bd7f7aef
NikoSalchak <nikosalchak@gmail.com>
2023-08-30 16:50:08 +0700

    6.0.1. Объявление массива. [#3613 #440857]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,11 +3,7 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        String[] names = new String[10500];
+        String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: ");
-        System.out.println("Размер массива равен " + ages.length);
-        System.out.println("Размер массива равен " + names.length);
-        System.out.println("Размер массива равен " + prices.length);
     }
 }
